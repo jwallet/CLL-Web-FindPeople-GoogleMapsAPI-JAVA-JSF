1f791354cdf931f9e15fcaec6f882c255da5f40b
José Ouellet <joseouellet@gmail.com>
2016-11-11 15:48:23 -0500

    initialisation

--- src/java/inscription/c.java
+++ src/java/inscription/c.java
@@ -5,6 +5,9 @@
  */
 package inscription;
 
+import com.googlecode.gmaps4jsf.component.map.Map;
+import com.googlecode.gmaps4jsf.component.marker.Marker;
+import com.googlecode.gmaps4jsf.component.eventlistener.EventListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,6 +21,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -29,16 +34,24 @@ public class c {
     private String userCodePostal = "";
     private String userPasse = "";
     
+    private String userNewEmail = "";
+    private String userNewCodePostal = "";
+    private String userNewPasse1 = "";
+    private String userNewPasse2 = "";
+
     private String userZoom = "12";
-    private String userGPSlon = "";
-    private String userGPSlat = "";
+    String userGPSlat = "46.8129233";
+    String userGPSlon = "-71.2044023";
+    private String visitorCodePostal = "";
+    
+    private Map map;
     
     private int dbIdCompte = 0;
     private String dbEmail = "";
     private String dbCodePostal = "";
     private String dbPasse = "";
-    private int dbGPSlat = 0;
-    private int dbGPSlon = 0;
+    private String dbGPSlat = "";
+    private String dbGPSlon = "";
     private int dbNoRue = 0;
     private String dbNomRue = "";
     private String dbVoisinage = "";
@@ -77,6 +90,15 @@ public class c {
         
     }
     
+    public String getDbEmail()
+    {
+        return dbEmail;
+    }
+    
+    public String getDbPasse()
+    {
+        return dbPasse;
+    }
     public boolean getConnecterStatus()
     {
         return connecter_succes;
@@ -87,6 +109,10 @@ public class c {
         userEmail = "";
         userCodePostal = "";
         userPasse = "";
+        userZoom = "12";
+        visitorCodePostal = "";
+        userGPSlon = "-71.2044023";
+        userGPSlat = "46.8129233";
         connecter_succes=false;
         return "index.xhtml";
     }
@@ -117,14 +143,50 @@ public class c {
     {
         this.userPasse = p1;
     }
-   
-    public void save() throws ClassNotFoundException, SQLException
-    {
+
+    public String getCodePostal(){
+        return userCodePostal;
+    }
+    
+    public void setCodePostal(String e){
+        this.userCodePostal = e;
+    }
+    
+    public String getTempCodePostal(){
+        return visitorCodePostal;
+    }
+    
+    public void setTempCodePostal(String e){
+        this.visitorCodePostal = e;
+    }
+    
+    public String getZoom(){
+        return userZoom;
+    }
+    public String getGPSlat(){
+        return userGPSlat;
+    }
+    
+    public String getGPSlon(){
+        return userGPSlon;
+    }
+    
+    public Map getMap(){
+        return map;
+    }
+    
+    public void setMap(Map map) throws ClassNotFoundException, SQLException{
+        //to set markers on map
+        //http://biemond.blogspot.ca/2008/09/google-maps-for-jsf-gmaps4jsf-in.html
+        this.map = map;  
+        map.setLatitude(userGPSlat);  
+        map.setLongitude(userGPSlon); 
+  
         ResultSet rs;
         PreparedStatement pst;
         Connection con = this.getConnexion();
         
-        String stm = "select * from compte";
+        String stm = "select idCompte, gpslat, gpslon, pseudo from compte";
       
         try
         {
@@ -133,11 +195,181 @@ public class c {
             rs = pst.getResultSet();
             while(rs.next()){
                 dbIdCompte = rs.getInt(1);
+                dbGPSlat = rs.getString(5);
+                dbGPSlon = rs.getString(6);
+                dbPseudo = rs.getString(15);
+                //note: ici je peux calculer si on ajoute un mark pour le membre si la distance gpsLON - usergpsLON est a l'interieur de 5 km.
+                //mark member on map
+                Marker mark = new Marker();  
+                mark.setLatitude(dbGPSlat);  
+                mark.setLongitude(dbGPSlon);  
+                mark.setJsVariable("memberMark"+dbIdCompte+"js");  
+                mark.setId("memberMark"+dbIdCompte);  
+                EventListener event = new EventListener();  
+                event.setEventName("clickMemberMark"+dbIdCompte);  
+                event.setJsFunction("clickHandlerMemberMark"+dbIdCompte+"js");  
+                mark.getChildren().add(event);  
+                map.getChildren().add(mark);  
+            }
+        }
+        catch (SQLException ex){
+            System.out.println("in exec");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public String getNewEmail()
+    {
+        return userNewEmail;
+    }
+    
+    public void setNewEmail(String e)
+    {
+        this.userNewEmail = e;
+    }
+    
+    public String getNewCPostal()
+    {
+        if(!visitorCodePostal.isEmpty())
+            userNewCodePostal = visitorCodePostal;
+        return userNewCodePostal;
+    }
+    
+    public void setNewCPostal(String g)
+    {
+        this.userNewCodePostal = g;
+    }
+    
+    public String getNewPasse1()
+    {
+        return userNewPasse1;
+    }
+    
+    public void setNewPasse1(String p1)
+    {
+        this.userNewPasse1 = p1;
+    }
+    
+    public String getNewPasse2()
+    {
+        return userNewPasse2;
+    }
+    
+    public void setNewPasse2(String p2)
+    {        
+        this.userNewPasse2 = p2;
+    }
+    
+    public void register() throws ClassNotFoundException, SQLException, JSONException, MalformedURLException, IOException
+    {        
+        boolean passeMatches = userNewPasse1.equals(userNewPasse2);
+        boolean codePostalValide = userNewCodePostal.matches("^(?!.*[DFIOQU])[A-VXY][0-9][A-Z][0-9][A-Z][0-9]$");
+
+        LocateGeoMe();
+        
+        if(passeMatches) //&& codePostalValide)
+        {
+            try
+            {
+                PreparedStatement prepareStatementSQLFormat;
+                Connection connectionToBD = this.getConnexion();
+
+                String statementSQL = "INSERT INTO compte(email,cpostal,passe,gpslat,gpslon) VALUES(?,?,?,?,?)"; //insert
+
+            
+                prepareStatementSQLFormat = connectionToBD.prepareStatement(statementSQL);
+                prepareStatementSQLFormat.setString(1, userNewEmail);
+                prepareStatementSQLFormat.setString(2, userNewCodePostal);           
+                prepareStatementSQLFormat.setString(3, userNewPasse1);
+                prepareStatementSQLFormat.setString(4, userGPSlat);
+                prepareStatementSQLFormat.setString(5, userGPSlon);
+                prepareStatementSQLFormat.executeUpdate(); 
+                
+            }
+            catch (SQLException ex){
+                System.out.println("in exec");
+                System.out.println(ex.getMessage());
+            }
+            
+            userEmail = userNewEmail;
+            userPasse = userNewPasse1;
+            userCodePostal = userNewCodePostal;
+            userNewEmail = "";
+            userNewPasse1= "";
+            userNewPasse2 = "";
+            userNewCodePostal = "";
+            
+            connect();
+        }       
+    }     
+    
+    private JSONObject GoogleApiToJson(String CodePostal) throws JSONException, MalformedURLException, IOException
+    {       
+        String sUrl = "http://maps.googleapis.com/maps/api/geocode/json?address="+ CodePostal + "&sensor=false";
+        URL url = new URL(sUrl);
+        URLConnection webConnectionToGoogleAPIgeocode = url.openConnection();
+        BufferedReader inBuffer;
+        inBuffer = new BufferedReader(new InputStreamReader(webConnectionToGoogleAPIgeocode.getInputStream()));
+        String inputLine;           
+        String jsonResult="";
+        while ((inputLine = inBuffer.readLine()) != null){
+            jsonResult += inputLine;                
+        }
+        inBuffer.close();
+        JSONObject o = new JSONObject(jsonResult);      
+        return o;
+    }
+    
+    private boolean valideJSON(JSONObject o) throws JSONException{
+        String status="";
+        if(o.has("status")) status = o.getString("status");
+        return status.equals("OK");
+    }
+    public void LocateGeoMe()throws JSONException, MalformedURLException, IOException
+    {
+        String CodePostal = userCodePostal;
+        if (userCodePostal.isEmpty())
+        {
+            CodePostal = visitorCodePostal;
+        }
+        JSONObject obj = GoogleApiToJson(CodePostal);
+        boolean r = valideJSON(obj);
+        while(!r && CodePostal.length()>=3)
+        {
+            CodePostal = CodePostal.substring(0, CodePostal.length()-3);
+            obj = GoogleApiToJson(CodePostal);
+            r = valideJSON(obj);  
+        }
+        if(CodePostal.length()==6)
+            userZoom = "15";
+        else
+            userZoom = "12";
+        userGPSlat = Double.toString(obj.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getDouble("lat"));
+        userGPSlon = Double.toString(obj.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getDouble("lng"));
+    }
+   
+    public void connect() throws ClassNotFoundException, SQLException
+    {
+        ResultSet rs;
+        PreparedStatement pst;
+        Connection con = this.getConnexion();
+        
+        String stm = "select * from compte where email=? and passe=?";
+      
+        try
+        {
+            pst = con.prepareStatement(stm);
+            pst.setString(1, userEmail);
+            pst.setString(2, userPasse);
+            pst.executeQuery();
+            rs = pst.getResultSet();
+            while(rs.next()){
+                dbIdCompte = rs.getInt(1);
                 dbEmail = rs.getString(2);
                 dbCodePostal = rs.getString(3);
                 dbPasse = rs.getString(4);
-                dbGPSlat = rs.getInt(5);
-                dbGPSlon = rs.getInt(6);
+                dbGPSlat = rs.getString(5);
+                dbGPSlon = rs.getString(6);
                 dbNoRue = rs.getInt(7);
                 dbNomRue = rs.getString(8);
                 dbVoisinage = rs.getString(9);
@@ -155,12 +387,44 @@ public class c {
         }
         
         connecter_succes = dbEmail.equals(userEmail) && dbPasse.equals(userPasse);
+        if(connecter_succes){
+            userCodePostal = dbCodePostal;
+            userPasse = dbPasse;
+            if(dbGPSlon.isEmpty() || dbGPSlat.isEmpty()){
+                updateDbLocation_ActuallyEmpty();
+            }
+            else{
+                userGPSlat = dbGPSlat;
+                userGPSlon = dbGPSlon;  
+            }
+        }       
 
-    }   
+    }       
     
-    public void recentrerSurCPostal() {
-        //sassurer de recentrer selon les donnees recueillis dans classe G et classe C
-        //trouver moyen d importer la classe
+    public void updateDbLocation_ActuallyEmpty() throws ClassNotFoundException{
+        try {
+                    LocateGeoMe();
+                } catch (JSONException | IOException ex) {
+                    Logger.getLogger(c.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                try
+                {
+                    PreparedStatement prepareStatementSQLFormat;
+                    Connection connectionToBD = this.getConnexion();
+
+                    String statementSQL = "UPDATE compte SET gpslat=?, gpslon=? WHERE email=? AND passe=?;"; 
+
+                    prepareStatementSQLFormat = connectionToBD.prepareStatement(statementSQL);
+                    prepareStatementSQLFormat.setString(1, userGPSlat);
+                    prepareStatementSQLFormat.setString(2, userGPSlon);
+                    prepareStatementSQLFormat.setString(3, userEmail);
+                    prepareStatementSQLFormat.setString(4, userPasse);
+                    prepareStatementSQLFormat.executeUpdate(); 
+
+                }
+                catch (SQLException ex){
+                    System.out.println("in exec");
+                    System.out.println(ex.getMessage());
+                }
     }
-    
 }
